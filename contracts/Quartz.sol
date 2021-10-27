e86a0b28b9e43d980a6280e4ca44c5e74630eccf
Miguel Palhas <mpalhas@gmail.com>
2021-10-27 18:31:09 +0100

    Writing some comments for Quartz and QuartzToken
    It was hard for me, as a newcomer, to follow the codebase at first,
    since it lacked comments or explanations
    
    This is my attempt to fix that for two of the contracts, and hopefully
    improve things for future teammates.
    
    I also found a couple of points of discussion along the way

--- contracts/Quartz.sol
+++ contracts/Quartz.sol
@@ -7,12 +7,23 @@ import "./interfaces/IQuartzGovernor.sol";
 import "./interfaces/IQuartz.sol";
 import "./interfaces/IChildToken.sol";
 
+/**
+ * Polygon version of our Quartz token, bridged from Ethereum
+ *
+ * @notice This token starts out with 0 supply. All minting is done by the
+ * bridge's ChildChainManager when a cross-chain transaction is made
+ *
+ * @notice In addition to ERC20 functionalities, this contract also allows
+ * holders to stake tokens, which grants them voting rights on `Governor`, or
+ * the ability to delegate that power to another party
+ */
 contract Quartz is
     ERC20("Sandclock", "QUARTZ"),
     AccessControl,
     IQuartz,
     IChildToken
 {
+    // Emitted when Quartz is staked
     event Staked(
         uint64 indexed id,
         address indexed owner,
@@ -21,6 +32,7 @@ contract Quartz is
         uint64 maturationTime
     );
 
+    // Emitted when Quartz is unstaked
     event Unstaked(
         uint64 indexed id,
         address indexed owner,
@@ -28,12 +40,14 @@ contract Quartz is
         uint256 amount
     );
 
+    // Emitted when the delegatee of an account is changed
     event DelegateChanged(
         address indexed delegator,
         address indexed fromDelegate,
         address indexed toDelegate
     );
 
+    // Emitted when voting power for an account is changed
     event DelegateVotesChanged(
         address indexed delegate,
         uint256 previousBalance,
@@ -49,26 +63,46 @@ contract Quartz is
         bool active; // Indicates active after maturation time
     }
 
+    // an update to voting power for an entity
     struct Checkpoint {
         uint32 fromBlock;
         uint256 votes;
     }
 
+    // role required by Polygon PoS bridge to mint tokens when a cross-chain transaction happens
+    // https://docs.polygon.technology/docs/develop/ethereum-polygon/pos/mapping-assets/
     bytes32 public constant DEPOSITOR_ROLE = keccak256("DEPOSITOR_ROLE");
 
     IQuartzGovernor public governor;
 
+    // total amount staked by an account, which corresponds to his total voting power
     mapping(address => uint256) public userVotesRep;
+
+    // delegates for each account
     mapping(address => address) public delegates;
+
+    // all checkpoints for all accounts
     mapping(address => mapping(uint32 => Checkpoint)) public checkpoints;
+
+    // checkpoint counters for all accounts
     mapping(address => uint32) public numCheckpoints;
+
+    // minimum period before tokens can be unstaked
     uint64 public minStakePeriod;
+
+    // stakes counter
     uint64 public stakeLength;
+
     // All stakes infos
     mapping(uint64 => StakeInfo) public stakes;
+
     // Total staked amount
     uint256 public override totalStaked;
 
+    /**
+     * @param _minStakePeriod the initial minStakePeriod to set
+     * @param _childChainManager ChildChainManager instance for Polygon PoS bridge
+     */
     constructor(uint64 _minStakePeriod, address _childChainManager) {
         minStakePeriod = _minStakePeriod;
 
@@ -80,6 +114,17 @@ contract Quartz is
         _setupRole(DEPOSITOR_ROLE, _childChainManager);
     }
 
+    //
+    // Public API
+    //
+
+    /**
+     * Sets the governor contract
+     *
+     * @param _governor new Governor instance to use
+     *
+     * @notice Can only be called by a contract admin
+     */
     function setGovernor(IQuartzGovernor _governor)
         external
         onlyRole(DEFAULT_ADMIN_ROLE)
@@ -91,7 +136,13 @@ contract Quartz is
         governor = _governor;
     }
 
-    // Stake QUARTZ token to grant vote rep to beneficiary for a period.
+    /**
+     * Stake QUARTZ token to grant vote rep to beneficiary for a period.
+     *
+     * @param _amount Amount of QUARTZ to stake
+     * @param _beneficiary Beneficiary account for this stake
+     * @param _period minimum period before unstaking is possible
+     */
     function stake(
         uint256 _amount,
         address _beneficiary,
@@ -141,6 +192,11 @@ contract Quartz is
         );
     }
 
+    /**
+     * Unstakes an existing stake
+     *
+     * @param _stakeId ID of the stake to unstake
+     */
     function unstake(uint64 _stakeId) external {
         require(_stakeId < stakeLength, "QUARTZ: Invalid id");
         StakeInfo storage stakeInfo = stakes[_stakeId];
@@ -176,12 +232,22 @@ contract Quartz is
 
     /**
      * @notice Delegate votes from `msg.sender` to `delegatee`
+     *
      * @param delegatee The address to delegate votes to
      */
     function delegate(address delegatee) external {
         return _delegate(msg.sender, delegatee);
     }
 
+    /**
+     * Locks voting power in the governor contract. Used by the governor
+     * contract when votes are cast, to lock them until the proposal is finished
+     *
+     * @notice Only callable by the governor contract
+     *
+     * @param user User to get votes from
+     * @param amount How many votes to lock
+     */
     function moveVotesToGovernor(address user, uint256 amount)
         external
         override
@@ -193,6 +259,15 @@ contract Quartz is
         _moveDelegates(user, msg.sender, amount);
     }
 
+    /**
+     * Unlocks voting power from the governor contract. Used by the governor
+     * contract when a proposal is finished, and all of its votes are unlocked
+     *
+     * @notice Only callable by the governor contract
+     *
+     * @param user User to get votes from
+     * @param amount How many votes to unlock
+     */
     function moveVotesFromGovernor(address user, uint256 amount)
         external
         override
@@ -205,7 +280,8 @@ contract Quartz is
     }
 
     /**
-     * @notice Gets the current votes balance for `account`
+     * Gets the current votes balance for `account`
+     *
      * @param account The address to get votes balance
      * @return The number of current votes for `account`
      */
@@ -220,6 +296,62 @@ contract Quartz is
             nCheckpoints > 0 ? checkpoints[account][nCheckpoints - 1].votes : 0;
     }
 
+    /**
+     * Updates the minimum period for new stakes
+     *
+     * @param _minStakePeriod new minumum period
+     */
+    function setMinStakePeriod(uint64 _minStakePeriod)
+        external
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        minStakePeriod = _minStakePeriod;
+    }
+
+    //
+    // Polygon PoS Bridge API
+    //
+
+    /**
+     * @notice called when token is deposited on root chain
+     *
+     * @dev Should be callable only by ChildChainManager
+     * Should handle deposit by minting the required amount for user
+     * Make sure minting is done only by this function
+     *
+     * @param _user user address for whom deposit is being done
+     * @param _depositData abi encoded amount
+     */
+    function deposit(address _user, bytes calldata _depositData)
+        external
+        override
+        onlyRole(DEPOSITOR_ROLE)
+    {
+        uint256 amount = abi.decode(_depositData, (uint256));
+        _mint(_user, amount);
+    }
+
+    /**
+     * @notice called when user wants to withdraw tokens back to root chain
+     *
+     * @dev Should burn user's tokens. This transaction will be verified when exiting on root chain
+     *
+     * @param _amount amount of tokens to withdraw
+     */
+    function withdraw(uint256 _amount) external override {
+        _burn(_msgSender(), _amount);
+    }
+
+    //
+    // Private logic
+    //
+
+    /**
+     * Sets a new delegate for an account, overwriting any previously existing delegate
+     *
+     * @param delegator Account to delegate from
+     * @param delegatee Account to delegate to
+     */
     function _delegate(address delegator, address delegatee) internal {
         require(delegatee != address(0), "QUARTZ: delegatee cannot be 0x0");
         address currentDelegate = delegates[delegator];
@@ -231,13 +363,26 @@ contract Quartz is
         _moveDelegates(currentDelegate, delegatee, delegatorVotesRep);
     }
 
+    /**
+     * Moves voting power from an old delegate to a new one
+     *
+     * @notice If no explicit delegate is registered, the account is actually
+     * its own delegate so this function is always used when transfering voting
+     * power
+     *
+     * @param srcRep Account from which to take votes. If 0x0, we're creating new voting power
+     * @param dstRep Account which will receive votes. If 0x0, we're destroying voting power
+     * @param amount Amount of votes to transfer
+     */
     function _moveDelegates(
         address srcRep,
         address dstRep,
         uint256 amount
     ) internal {
+        // if both addresses are the same, or amount == 0, this is ano-op
         if (srcRep != dstRep && amount > 0) {
             if (srcRep != address(0)) {
+                // remove voting power from srcRep
                 uint32 srcRepNum = numCheckpoints[srcRep];
                 uint256 srcRepOld =
                     srcRepNum > 0
@@ -259,6 +404,7 @@ contract Quartz is
             }
 
             if (dstRep != address(0)) {
+                // add voting power to dstRep
                 uint32 dstRepNum = numCheckpoints[dstRep];
                 uint256 dstRepOld =
                     dstRepNum > 0
@@ -270,6 +416,13 @@ contract Quartz is
         }
     }
 
+    /**
+     * Writes a new checkpoint with updated voting power info for a given delegatee
+     *
+     * @param delegatee The delegatee account
+     * @param nCheckpoints How many checkpoints already exist for this delegatee
+     * @param oldVotes
+     */
     function _writeCheckpoint(
         address delegatee,
         uint32 nCheckpoints,
@@ -298,6 +451,13 @@ contract Quartz is
         emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
     }
 
+    /**
+     * Safely casts uint256 to uint32, reverting if it doesn't fit
+     *
+     * @param n Number to check
+     * @param errorMessage message to throw in case of error
+     * @return the converted uint32
+     */
     function safe32(uint256 n, string memory errorMessage)
         internal
         pure
@@ -307,40 +467,10 @@ contract Quartz is
         return uint32(n);
     }
 
+    /**
+     * Returns the current block timestamp as a uint64
+     */
     function _getBlockTimestamp() private view returns (uint64) {
         return uint64(block.timestamp);
-    }
-
-    function setMinStakePeriod(uint64 _minStakePeriod)
-        external
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        minStakePeriod = _minStakePeriod;
-    }
-
-    /**
-     * @notice called when token is deposited on root chain
-     * @dev Should be callable only by ChildChainManager
-     * Should handle deposit by minting the required amount for user
-     * Make sure minting is done only by this function
-     * @param _user user address for whom deposit is being done
-     * @param _depositData abi encoded amount
-     */
-    function deposit(address _user, bytes calldata _depositData)
-        external
-        override
-        onlyRole(DEPOSITOR_ROLE)
-    {
-        uint256 amount = abi.decode(_depositData, (uint256));
-        _mint(_user, amount);
-    }
-
-    /**
-     * @notice called when user wants to withdraw tokens back to root chain
-     * @dev Should burn user's tokens. This transaction will be verified when exiting on root chain
-     * @param _amount amount of tokens to withdraw
-     */
-    function withdraw(uint256 _amount) external override {
-        _burn(_msgSender(), _amount);
     }
 }
--- contracts/QuartzToken.sol
+++ contracts/QuartzToken.sol
@@ -3,6 +3,15 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+ * Quartz token on Ethereum blockchain
+ *
+ * @notice This token will be bridged to an instance of `Quartz` deployed on
+ * Polygon network.  Governance happens on the Polygon chain. As such, no
+ * additional logic is needed on this contract
+ *
+ * @notice Bridging to polygon is done via Polygon PoS
+ */
 contract QuartzToken is ERC20("Sandclock", "QUARTZ") {
     constructor() {
         _mint(msg.sender, 1e8 ether);
